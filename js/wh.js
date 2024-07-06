4415db9754e41c712eaf4fc24d46eef4ad23fd87
zh-hlj <160156519+zh-hlj@users.noreply.github.com>
2024-07-06 12:24:11 +0800

    Add files via upload

---
+++ js/wh.js
@@ -0,0 +1,22 @@
+function updateTime() {
+    var startTime = new Date('2024-07-06T00:00:00').getTime();
+    var endTime = new Date('2024-07-07T00:00:00').getTime();
+    var now = new Date().getTime();
+
+    var distance = endTime - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById('countdown').innerHTML = days + "天 " + hours + "小时 "
+    + minutes + "分钟 " + seconds + "秒 ";
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById('countdown').innerHTML = "维护已完成";
+    }
+}
+
+var x = setInterval(updateTime, 1000);
