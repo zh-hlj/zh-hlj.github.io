86d0ef560cf011bc40d597e81c74426e878a6768
zh-hlj <160156519+zh-hlj@users.noreply.github.com>
2025-01-11 09:41:36 +0800

    Add files via upload

--- js/download.js
+++ js/download.js
@@ -1,11 +1,11 @@
-document.addEventListener('DOMContentLoaded', () => {//定义下载链接
+document.addEventListener('DOMContentLoaded', () => {//定义网盘下载链接监听器
     const gameDownloadLink = document.getElementById('NEKOPARA_download');
     if (gameDownloadLink) {
         gameDownloadLink.addEventListener('click', (event) => {
             event.preventDefault();
             selectCloudServiceForGame();
         });
-    }//定义下载链接到此结束
+    }//定义网盘下载链接到此结束
 
     const animeLinks2024 = document.querySelectorAll('#anime_202401-12');//定义2024年动画情报下载链接
     animeLinks2024.forEach(link => {
@@ -22,12 +22,20 @@ document.addEventListener('DOMContentLoaded', () => {//定义下载链接
             selectCloudServiceForAnime2025();
         });
     });
+
+    const animeLinks2026 = document.querySelectorAll('#anime_202601-12'); // 定义2026年动画情报下载链接
+    animeLinks2026.forEach(link => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+            selectCloudServiceForAnime2026();
+        });
+    });
 });//定义2025年动画情报下载链接到此结束
 
 function selectCloudServiceForGame() {//网盘服务选择
     const services = [
         { name: '百度网盘', url: 'https://pan.baidu.com/s/14Gxqzi2NZE3q4lsruwl6sg?pwd=tzsg' },
-        { name: '夸克网盘（提取码：sLBM）', url: 'https://pan.quark.cn/s/c654142b4e34' },
+        { name: '夸克网盘（提取码：bBPJ）', url: 'https://pan.quark.cn/s/a6211fdada88' },
     ];
 
     let serviceList = '请选择一个网盘服务:\n';
@@ -98,3 +106,30 @@ function selectCloudServiceForAnime2025() {//2025年动画情报服务选择
         alert('未输入值或无效的选择');
     }
 }//2025年动画情报服务选择到此结束
+
+function selectCloudServiceForAnime2026() { // 2026年动画情报服务选择
+    const services = [
+        { name: '第一季度', url: '' },
+        { name: '第二季度', url: '' },
+        { name: '第三季度', url: '' },
+        { name: '第四季度', url: '' },
+    ];
+
+    let serviceList = '请选择月份:\n';
+    services.forEach((service, index) => {
+        serviceList += `${index + 1}. ${service.name}\n`;
+    });
+
+    const choice = prompt(serviceList);
+    const selectedService = services[parseInt(choice) - 1];
+
+    if (selectedService) {
+        if (selectedService.url) {
+            window.location.href = selectedService.url;
+        } else {
+            alert('该季度文件暂未整理，请等待后续更新或访问相关百科网站查找');
+        }
+    } else {
+        alert('未输入值或无效的选择');
+    }
+} // 2026年动画情报服务选择到此结束
